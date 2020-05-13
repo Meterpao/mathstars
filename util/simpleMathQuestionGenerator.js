5b166605513d903fbe55b9df486e5ffdf1cad7d4
Dao Han Lim <dao_han_lim@brown.edu>
2020-05-13 02:10:25 -0400

    dont export helper functions

--- util/simpleMathQuestionGenerator.js
+++ util/simpleMathQuestionGenerator.js
@@ -2,39 +2,39 @@
 
 // returns first int, second int and the summation of the two
 // first int and second int will be between 1 to 10 inclusive
-module.exports.simpleMathQuestionGenerator = function randomAdditionQuestion() {
+function randomAdditionQuestion() {
   var firstNumber = Math.floor(Math.random() * 10) + 1;
   var secondNumber = Math.floor(Math.random() * 10) + 1;
-  return firstNumber, "+", secondNumber, firstNumber + secondNumber;
-};
+  return [firstNumber, "+", secondNumber, firstNumber + secondNumber];
+}
 
 // returns first int, second int and the subtraction of the first - the second
 // first int and second int will be between 1 to 10 inclusive
-module.exports.simpleMathQuestionGenerator = function randomSubtractionQuestion() {
+function randomSubtractionQuestion() {
   var firstNumber = Math.floor(Math.random() * 10) + 1;
   var secondNumber = Math.floor(Math.random() * 10) + 1;
   return firstNumber, "-", secondNumber, firstNumber - secondNumber;
-};
+}
 
 // returns first int, second int and the multiplication of the two
 // first int and second int will be between 1 to 10 inclusive
-module.exports.simpleMathQuestionGenerator = function randomMultiplicationQuestion() {
+function randomMultiplicationQuestion() {
   var firstNumber = Math.floor(Math.random() * 10) + 1;
   var secondNumber = Math.floor(Math.random() * 10) + 1;
-  return firstNumber, "x", secondNumber, firstNumber * secondNumber;
-};
+  return [firstNumber, "x", secondNumber, firstNumber * secondNumber];
+}
 
 // returns a * b, a, b
 // first int and second int will be between 1 to 10 inclusive
-module.exports.simpleMathQuestionGenerator = function randomDivisionQuestion() {
+function randomDivisionQuestion() {
   var firstNumber = Math.floor(Math.random() * 10) + 1;
   var secondNumber = Math.floor(Math.random() * 10) + 1;
-  return firstNumber * secondNumber, "/", secondNumber, firstNumber;
-};
+  return [firstNumber * secondNumber, "/", secondNumber, firstNumber];
+}
 
 // returns a random math question
 // inputs: includesOperator - if this is 1 then it will be included as a possible outcome
-module.exports.simpleMathQuestionGenerator = function randomSimpleQuestion(
+function randomSimpleQuestion(
   includeAddition = 1,
   includeSubtraction = 1,
   includeMultiplication = 1,
@@ -43,21 +43,21 @@ module.exports.simpleMathQuestionGenerator = function randomSimpleQuestion(
   var randomQuestionInt = Math.random() * arguments.reduce((a, b) => a + b, 0);
   var runningSum = includeAddition;
   if (randomQuestionInt <= runningSum) {
-    return randomAdditionQuestion(), "+";
+    return randomAdditionQuestion();
   }
   runningSum += includeSubtraction;
   if (randomQuestionInt <= runningSum) {
-    return randomSubtractionQuestion(), "-";
+    return randomSubtractionQuestion();
   }
   runningSum += includeMultiplication;
   if (randomQuestionInt <= runningSum) {
-    return randomMultiplicationQuestion(), "x";
+    return randomMultiplicationQuestion();
   }
   runningSum += includeDivision;
   if (randomQuestionInt <= runningSum) {
-    return randomDivisionQuestion(), "/";
+    return randomDivisionQuestion();
   }
-};
+}
 
 module.exports.simpleMathQuestionGenerator = function getQuestions(
   includeAddition = 1,
